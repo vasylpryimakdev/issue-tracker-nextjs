6d44696ed5d7bbbae23d826e888f25dcdd908f5b
vasylpryimakdev <vasyl.pryimak.dev@gmail.com>
2026-03-06 23:00:26 +0200

    Show toast notification

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -12,6 +12,7 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Skeleton } from "@/app/components";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -34,27 +35,34 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   }
 
   return (
-    <SelectRoot
-      defaultValue={issue.assignedToUserId || ""}
-      onValueChange={(userId) =>
-        axios.patch(`/api/issues/${issue.id}`, {
-          assignedToUserId: userId || null,
-        })
-      }
-    >
-      <SelectTrigger placeholder="Assing..."></SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Suggestions</SelectLabel>
-          <SelectItem value="">Unassigned</SelectItem>
-          {users?.map((user) => (
-            <SelectItem key={user.id} value={user.id}>
-              {user.name}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </SelectRoot>
+    <>
+      <SelectRoot
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={(userId) =>
+          axios
+            .patch(`/api/issues/${issue.id}`, {
+              assignedToUserId: userId || null,
+            })
+            .catch(() => {
+              toast.error("Changes could not be saved");
+            })
+        }
+      >
+        <SelectTrigger placeholder="Assing..."></SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Suggestions</SelectLabel>
+            <SelectItem value="">Unassigned</SelectItem>
+            {users?.map((user) => (
+              <SelectItem key={user.id} value={user.id}>
+                {user.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </SelectRoot>
+      <Toaster />
+    </>
   );
 };
 
