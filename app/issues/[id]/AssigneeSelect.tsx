85f66f9a241b4263dea905bf66c7e6b98a5f9c7b
vasylpryimakdev <vasyl.pryimak.dev@gmail.com>
2026-03-06 22:13:21 +0200

    Fetch data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -9,20 +9,29 @@ import {
   SelectRoot,
   SelectTrigger,
 } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { Skeleton } from "@/app/components";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: [],
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+    staleTime: 10 * 60,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fethcUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
+  if (isLoading) {
+    return <Skeleton />;
+  }
 
-    fethcUsers();
-  }, []);
+  if (error) {
+    return null;
+  }
 
   return (
     <SelectRoot>
@@ -30,7 +39,7 @@ const AssigneeSelect = () => {
       <SelectContent>
         <SelectGroup>
           <SelectLabel>Suggestions</SelectLabel>
-          {users.map((user) => (
+          {users?.map((user) => (
             <SelectItem key={user.id} value={user.id}>
               {user.name}
             </SelectItem>
