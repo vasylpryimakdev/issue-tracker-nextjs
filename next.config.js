75df49d5e47252928698d40fcb96d03dc9bd053b
vasylpryimakdev <vasyl.pryimak.dev@gmail.com>
2026-03-09 15:07:31 +0200

    fix path of authOptions file  for deploy

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   async headers() {
-      return [
-         {
-            source: '/:path*',
-            headers: [
-               { key: 'referrer-policy', value: 'no-referrer'}
-            ]
-         }
-      ]
-   }
-}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'referrer-policy', value: 'no-referrer' }
+        ]
+      }
+    ]
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
